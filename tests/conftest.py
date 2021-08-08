6dc656379cf06a098e67f92e6dad3fceb09c361c
Carlo De Pieri <depieri.carlo@gmail.com>
2021-08-08 10:00:31 +0200

    start the project

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+import shutil
+
+
+test_cassettes_folder = "tests/cassettes"
+
+
+#
+# ensure the cassettes folder used for testing is cleared before every test
+#
+@pytest.fixture(scope="function", autouse=True)
+def clear_cassettes():
+    if os.path.isdir(test_cassettes_folder):
+        shutil.rmtree(test_cassettes_folder)
+    yield
+
+
+def run_test(test_string: str) -> int:
+    """Write the test string to a temporary unique file and run pytest on it. Return the exit code."""
+    # make sure the test file is unique - this will prevent caching
+    test_file_path = f"tests/test_temp_{hash(test_string)}.py"
+    with open(test_file_path, "w") as f:
+        f.write(test_string)
+    ret_code = pytest.main(["-x", test_file_path])
+    os.remove(test_file_path)
+    return ret_code
---
+++ tests/test_test_module.py
@@ -0,0 +1,49 @@
+import os
+import pytest
+import requests
+import textwrap
+import vcr
+
+from tests.conftest import run_test, test_cassettes_folder
+
+
+cassette_file = f"{test_cassettes_folder}/test.yaml"
+
+
+def get_test_string(passing: bool) -> str:
+    return textwrap.dedent(f"""
+                import requests
+                import vcr
+                
+                
+                class TestCollection:
+                    def test_this(self):
+                        with vcr.use_cassette("{cassette_file}"):
+                            requests.get("https://github.com")
+                        assert {"True" if passing else "False"}
+                """)
+
+
+def there_are_no_temp_file() -> bool:
+    return len(list(filter(lambda f: f.startswith("test_temp"), os.listdir("tests")))) == 0
+
+
+class TestTheTestModule:
+    """Test: The test module..."""
+
+    @pytest.mark.parametrize("test_string,return_code", [(get_test_string(True), 0), (get_test_string(False), 1)])
+    def test_should_be_able_to_run_arbitrary_test_files(self, test_string, return_code):
+        """The module should be able to run arbitrary test files."""
+        ret_code = run_test(test_string)
+        assert ret_code == return_code
+        assert there_are_no_temp_file()
+        assert os.path.isfile(cassette_file)
+
+    # Order is not deterministic, hence this double test with the dummy parametrization
+    @pytest.mark.parametrize("dummy", [True, True])
+    def test_should_ensure_that_no_cassette_are_there_at_the_start_of_a_test_a(self, dummy):
+        """The test module should ensure that no cassette are there at the start of a test a."""
+        common_cassette = f"{test_cassettes_folder}/common.yaml"
+        assert not os.path.isfile(common_cassette)
+        with vcr.use_cassette(f"{common_cassette}"):
+            requests.get("https://github.com")
